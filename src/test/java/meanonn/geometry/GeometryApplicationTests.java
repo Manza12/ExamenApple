ec6d2bcc01ae006c3bcc412f2843b04ede04afa6
Manza12 <tritery@hotmail.com>
2023-01-09 19:36:38 +0100

    Initial commit

---
+++ src/test/java/meanonn/geometry/GeometryApplicationTests.java
@@ -0,0 +1,13 @@
+package meanonn.geometry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeometryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
